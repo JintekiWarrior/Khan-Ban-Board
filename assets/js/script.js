517c2fd2aecbbe115636dad046360299659f16ab
Moe Yassine <moeyassine1993@gmail.com>
2022-02-23 13:51:42 -0500

    Add prevent default to event handler - 4.2.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
+const formEl = document.querySelector("#task-form")
 const tasksToDo = document.querySelector("#tasks-to-do")
-const buttonEl = document.querySelector("#save-task")
 
-const createTaskHandler = function() {
+const createTaskHandler = function(event) {
+  event.preventDefault()
+
   const listItemEl = document.createElement("li")
   listItemEl.className = "task-item"
   listItemEl.textContent = "This is a new task."
   tasksToDo.appendChild(listItemEl)
 }
 
-buttonEl.addEventListener("click", createTaskHandler())
-
-document.createElement("li")
+formEl.addEventListener("submit", createTaskHandler)
