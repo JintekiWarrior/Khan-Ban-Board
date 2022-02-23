c354f7362cba337cee63b57cf4bafa64805b45a2
Moe Yassine <moeyassine1993@gmail.com>
2022-02-23 17:30:09 -0500

    Add select option functionality - 4.2.6

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,17 @@ const tasksToDo = document.querySelector("#tasks-to-do")
 const createTaskHandler = function(event) {
   event.preventDefault()
 
+  const taskNameInput = document.querySelector("input[name='task-name']").value
+  const taskTypeInput = document.querySelector("select[name='task-type']").value
+
   const listItemEl = document.createElement("li")
   listItemEl.className = "task-item"
-  listItemEl.textContent = "This is a new task."
+
+  const taskInfoEl = document.createElement("div")
+  taskInfoEl.className = "task-info"
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"
+
+  listItemEl.appendChild(taskInfoEl)
   tasksToDo.appendChild(listItemEl)
 }
 
